6a1abc68213bad4d389be1c684e7df00ae4de7a7
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-03-14 20:45:21 +0100

    Added audio list

---
+++ src/components/AudioList/AudioList.jsx
@@ -0,0 +1,108 @@
+import styles from "./AudioList.module.css";
+import AudioImage from "../../static/AudioImage.png";
+import { AiFillPlayCircle } from "react-icons/ai";
+
+function AudioList() {
+    return (
+        <div className={styles.audios}>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+            <div className={styles.audioCard}>
+                <img src={AudioImage} alt="Audio Card" />
+                <h3>Runnin</h3>
+                <h4>Pharell Williams</h4>
+                <div className={styles.audioHover}>
+                    <button type="button"><AiFillPlayCircle size="3rem" className={styles.play}/></button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AudioList;
