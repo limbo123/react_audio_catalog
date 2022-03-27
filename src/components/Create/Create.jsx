b9ed9748525dca104f11d2ae69a1b6b0c3c82ffb
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-03-27 19:10:44 +0200

    Added showing uploaded image and audio

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -8,6 +8,8 @@ const INITIAL_STATE = {
   title: '',
   author: '',
   genres: '',
+  uploadImg: null,
+  uploadAudioName: null,
 }
 
 class CreateForm extends React.Component {
@@ -19,6 +21,18 @@ class CreateForm extends React.Component {
     this.setState({ [name]: value })
   };
 
+  handleUploadImg = event => {
+    this.setState({
+      uploadImg: URL.createObjectURL(event.target.files[0])
+    });
+  }
+
+  handleUploadAudio = event => {
+    this.setState({
+      uploadAudioName: event.target.files[0].name,
+    });
+  }
+
   handleSubmit = evt => {
     evt.preventDefault()
 
@@ -29,7 +43,7 @@ class CreateForm extends React.Component {
       .then(data => console.log(data))
       .catch(error => console.error(error));
 
-    this.reset()
+    this.reset();
   }
 
   reset = () => {
@@ -41,6 +55,7 @@ class CreateForm extends React.Component {
 
     return (
       <form className={styles.Container} onSubmit={this.handleSubmit} name="createForm" encType='multipart/form-data'>
+
         <h2>{this.props.t("Enter Song Information")}:</h2>
 
         <input
@@ -49,10 +64,15 @@ class CreateForm extends React.Component {
           className="form-input visually-hidden"
           id="createImageInput"
           name='image'
+          onChange={this.handleUploadImg}
           required
         />
         <label className={styles.CreateFormBtn2} htmlFor="createImageInput">
+          <AiOutlineCloudUpload className={styles.CreateFormUploadImg} />
+
           {this.props.t("Upload File")}
+
+          <img src={this.state.uploadImg} alt="" className={styles.uploadImage} />
         </label>
 
         <div className={styles.CreateForm}>
@@ -93,9 +113,17 @@ class CreateForm extends React.Component {
             />
           </label>
 
-          <div>
+          <div className={styles.uploadAudio}>
+            <div>
+              <label htmlFor="file-input">
+                <AiOutlineCloudUpload size="1.5rem" />
+              </label>
+              <label htmlFor="file-input">
+                <p className={styles.uploadAudioTip}>{this.props.t("Upload Audio")}</p>
+              </label>
+            </div>
             <label htmlFor="file-input">
-              <AiOutlineCloudUpload size="1.5rem" />
+              <p className={styles.uploadAudioName}>{this.state.uploadAudioName}</p>
             </label>
             <input
               className={`${styles.CreateFormInput} ${styles.visuallyHidden}`}
@@ -103,6 +131,7 @@ class CreateForm extends React.Component {
               id="file-input"
               name="audio"
               accept='audio/*'
+              onChange={this.handleUploadAudio}
               required
             />
           </div>
@@ -116,5 +145,4 @@ class CreateForm extends React.Component {
   }
 }
 
-// ReactDOM.render(Create, document.getElementById('root'))
 export default withTranslation()(CreateForm);
