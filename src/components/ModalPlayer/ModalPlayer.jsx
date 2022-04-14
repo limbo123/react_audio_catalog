80aff0d298ec2326e2aa31b725b1c5af529abfe0
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-04-14 10:14:15 +0200

    Added super mini-player

--- src/components/ModalPlayer/ModalPlayer.jsx
+++ src/components/ModalPlayer/ModalPlayer.jsx
@@ -8,6 +8,7 @@ import {
   BsFillSkipStartFill,
 } from "react-icons/bs";
 import { FiMinimize2 } from "react-icons/fi";
+import { HiOutlineArrowsExpand } from "react-icons/hi";
 import formatTime from "../../formatTime";
 
 import styles from "./ModalPlayer.module.css";
@@ -19,7 +20,7 @@ const ModalPlayer = ({ handleModal, handleMini, isModMax, audios, trackIndex }) 
   const [isPlaying, setIsPlaying] = useState(true);
   const [time, setTime] = useState(0);
   const audioElement = useRef(null);
-  
+
 
   const timeUpdate = () => {
     setTime(audioElement.current.currentTime);
@@ -77,54 +78,61 @@ const ModalPlayer = ({ handleModal, handleMini, isModMax, audios, trackIndex }) 
 
   return createPortal(
     <div className={isModMax ? styles.Overlay : styles.Overlay_mini}>
-      <h6 className={isModMax ? styles.ModalTitle : styles.ModalTitle_mini}>NOW PLAYING</h6>
-      <div className={isModMax ? styles.CloseModalBtn : styles.CloseModalBtn_mini} onClick={handleMini}>
-        <FiMinimize2 size="1.6rem" />
-      </div>
+      <h6 className={isModMax ? styles.ModalTitle : styles.ModalTitle_mini}>NOW PLAYING:</h6>
+      {isModMax ?
+        <div className={styles.CloseModalBtn} onClick={handleMini}>
+          <FiMinimize2 size="1.6rem" />
+        </div> :
+        <div className={styles.CloseModalBtn_mini} onClick={handleMini}>
+          <HiOutlineArrowsExpand size="1.6rem" />
+        </div>}
       <div className={isModMax ? styles.SongDetails : styles.SongDetails_mini}>
         <img className={isModMax ? styles.SongImage : styles.SongImage_mini} src={audios[currentSongIndex].imageUrl} alt="" />
-        <h6 className={isModMax ? styles.SongTitle : styles.SongTitle_mini}>{audios[currentSongIndex].title}</h6>
-        <p className={isModMax ? styles.SongAuthor : styles.SongAuthor_mini}>{audios[currentSongIndex].author}</p>
+        <div>
+          <h6 className={isModMax ? styles.SongTitle : styles.SongTitle_mini}>{audios[currentSongIndex].title}</h6>
+          <p className={isModMax ? styles.SongAuthor : styles.SongAuthor_mini}>{audios[currentSongIndex].author}</p>
+        </div>
       </div>
+      <div className={isModMax ? styles.PlayerSettings : styles.PlayerSettings_mini}>
+        <div className={isModMax ? styles.PlayerTimeline : styles.PlayerTimeline_mini}>
+          <span className={isModMax ? styles.playerTimeCurrent : styles.playerTimeCurrent_mini}>{formattedTime}</span>
+          <input
+            type="range"
+            className={isModMax ? styles.playerTimeControl : styles.playerTimeControl_mini}
+            value={time}
+            max={audioElement?.current?.duration || 0}
+            onChange={(e) =>
+              (audioElement.current.currentTime = e.currentTarget.value)
+            }
+            style={{
+              background: `linear-gradient(to right, #fff ${timePercent}%, rgba(255, 255, 255, 0.3) ${timePercent}%)`,
+            }}
+          />
+          <span className={styles.playerTimeDuration}>{formattedDuration}</span>
+        </div>
 
-      <div className={isModMax ? styles.PlayerTimeline : styles.PlayerTimeline_mini}>
-        <span className={isModMax ? styles.playerTimeCurrent : styles.playerTimeCurrent_mini}>{formattedTime}</span>
-        <input
-          type="range"
-          className={isModMax ? styles.playerTimeControl : styles.playerTimeControl_mini}
-          value={time}
-          max={audioElement?.current?.duration || 0}
-          onChange={(e) =>
-            (audioElement.current.currentTime = e.currentTarget.value)
-          }
-          style={{
-            background: `linear-gradient(to right, #fff ${timePercent}%, rgba(255, 255, 255, 0.3) ${timePercent}%)`,
-          }}
-        />
-        <span className={styles.playerTimeDuration}>{formattedDuration}</span>
-      </div>
-
-      <div className={isModMax ? styles.PlayerControls : styles.PlayerControls_mini}>
-        <audio
-          src={audios[currentSongIndex].audioUrl}
-          ref={audioElement}
-        ></audio>
-        <button className={isModMax ? styles.SkipBackBtn : styles.SkipBackBtn_mini} onClick={() => skipSong(false)}>
-          <BsFillSkipStartFill color="#fff" size="1.3rem" />
-        </button>
-        <button
-          className={isModMax ? styles.PlayBtn : styles.PlayBtn_mini}
-          onClick={() => setIsPlaying(!isPlaying)}
-        >
-          {isPlaying ? (
-            <BsFillPauseFill color="#fff" size="2rem" />
-          ) : (
-            <BsPlayFill color="#fff" size="2rem" />
-          )}
-        </button>
-        <button className={isModMax ? styles.SkipBackBtn : styles.SkipBackBtn_mini} onClick={() => skipSong()}>
-          <BsFillSkipEndFill color="#fff" size="1.3rem" />
-        </button>
+        <div className={isModMax ? styles.PlayerControls : styles.PlayerControls_mini}>
+          <audio
+            src={audios[currentSongIndex].audioUrl}
+            ref={audioElement}
+          ></audio>
+          <button className={isModMax ? styles.SkipBackBtn : styles.SkipBackBtn_mini} onClick={() => skipSong(false)}>
+            <BsFillSkipStartFill color="#fff" size="1.3rem" />
+          </button>
+          <button
+            className={isModMax ? styles.PlayBtn : styles.PlayBtn_mini}
+            onClick={() => setIsPlaying(!isPlaying)}
+          >
+            {isPlaying ? (
+              <BsFillPauseFill color="#fff" size="2rem" />
+            ) : (
+              <BsPlayFill color="#fff" size="2rem" />
+            )}
+          </button>
+          <button className={isModMax ? styles.SkipBackBtn : styles.SkipBackBtn_mini} onClick={() => skipSong()}>
+            <BsFillSkipEndFill color="#fff" size="1.3rem" />
+          </button>
+        </div>
       </div>
     </div>,
     modalRoot
