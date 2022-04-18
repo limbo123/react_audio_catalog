0ce1539d7683a481174dee4b1533d5845a515dd1
Evgen <nazarenkoevgen99@gmail.com>
2022-04-18 19:16:36 +0300

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,78 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import "./App.css";
+import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import './App.css'
 
-import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./pages/SearchPage/SearchPage";
-import AddSongPage from "./pages/AddSongPage/AddSongPage";
-import Navbar from "./components/Navbar/Navbar";
-import ModalPlayer from "./components/ModalPlayer/ModalPlayer";
-<<<<<<< HEAD
+import HomePage from './pages/HomePage/HomePage'
+import SearchPage from './pages/SearchPage/SearchPage'
+import AddSongPage from './pages/AddSongPage/AddSongPage'
+import Navbar from './components/Navbar/Navbar'
+import ModalPlayer from './components/ModalPlayer/ModalPlayer'
+import routes from './routes'
+import NavbarMobile from './components/NavbarMobile/NavbarMobile'
 
-import routes from "./routes";
-=======
-import routes from "./routes";
-import NavbarMobile from "./components/NavbarMobile/NavbarMobile";
->>>>>>> origin/zavodskiyKV
-
-export default class App extends React.Component {    
+export default class App extends React.Component {
   state = {
-    currentLanguage: "",
+    currentLanguage: '',
     isModalOpened: false,
-<<<<<<< HEAD
-    isModalMaximized: true,
-    playerTrackIndex: 0,
-    audiosArray: [],
-  };
-
-=======
-  };
->>>>>>> origin/zavodskiyKV
+  }
 
   componentDidMount() {
     this.setState({
-      currentLanguage: localStorage.getItem("language"),
-    });
+      currentLanguage: localStorage.getItem('language'),
+    })
   }
 
   handleMini = () => {
     if (this.state.isModalMaximized) {
-      console.log(false);
+      console.log(false)
       this.setState(() => ({
         isModalMaximized: false,
-      }));
+      }))
     } else {
-      console.log(true);
+      console.log(true)
       this.setState(() => ({
         isModalMaximized: true,
-      }));
+      }))
     }
 
     if (this.state.isModalOpened) {
     } else {
-      console.log(true);
+      console.log(true)
       this.setState(() => ({
         isModalMaximized: true,
-      }));
+      }))
     }
 
     setTimeout(() => {
       if (this.state.isModalMaximized) {
-        document.body.style.overflow = "hidden";
+        document.body.style.overflow = 'hidden'
       } else {
-        document.body.style.overflow = "visible";
+        document.body.style.overflow = 'visible'
       }
-    }, 50);
-  };
+    }, 50)
+  }
 
   handleModal = (currentIndex, audiosArray) => {
-
     if (this.state.isModalOpened === false) {
-
       this.setState((prevState) => ({
         isModalOpened: true,
         playerTrackIndex: currentIndex,
         audiosArray,
-      }));
+      }))
     } else {
       this.setState((prevState) => ({
         isModalOpened: false,
         playerTrackIndex: currentIndex,
         audiosArray,
-      }));
+      }))
     }
-  };
+  }
 
   setLanguage = (lang) => {
     this.setState({
       currentLanguage: lang,
-    });
-  };
+    })
+  }
 
   render() {
     return (
@@ -102,7 +87,7 @@ export default class App extends React.Component {
           />
         )}
 
-        {this.state.currentLanguage !== "" && (
+        {this.state.currentLanguage !== '' && (
           <Navbar
             language={this.state.currentLanguage}
             setLang={this.setLanguage}
@@ -115,21 +100,12 @@ export default class App extends React.Component {
             exact
             render={(props) => (
               <HomePage {...props} handleModal={this.handleModal} />
-<<<<<<< HEAD
-            )}
-          />
-          <Route
-            path={routes.search}
-            component={(props) => (
-              <SearchPage {...props} handleModal={this.handleModal} />
-=======
->>>>>>> origin/zavodskiyKV
             )}
           />
           <Route path={routes.addSong} component={AddSongPage} />
           <Redirect to={routes.home} />
         </Switch>
       </>
-    );
+    )
   }
 }
